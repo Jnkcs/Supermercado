79d026fa76ced2b86f94e2757cd2485198c47f76
Jnkcs <65582227+Jnkcs@users.noreply.github.com>
2020-05-18 21:46:06 -0600

    Creacion de Archivo
    Se agrega el archivo para creacion de ficheros

---
+++ proyecto final.py
@@ -0,0 +1,58 @@
+
+def IngresoDeProd():
+    
+    int=input(var1)
+    
+    ProductosExistentes=(Nombre_Del_Producto", "Costo_De_Fabricacion", "Precio_De_Venta", "Unidades, Ganancia")
+    print (ProductosExistentes [int input(Var1):]);
+    import os
+    
+    os.system("pause")
+    os.system("cls")
+    
+    
+    
+def resta ():
+   
+    input()
+
+def multiplicacion():
+    
+    input()
+
+def division ():
+   
+    input()
+
+
+while (True):
+    print ("_______________________________________________________ ");
+    print ("l                                                      l");
+    print ("l             Seleccione:                              l");
+    print ("l     1...... Ingreso de nuevos productos              l");
+    print ("l     2...... Muestra los productos existetes          l");
+    print ("l     3...... Aniadir unidades a productos registrados l");
+    print ("l     4...... Modificacion de Productos                l");
+    print ("l     5...... SALIR                                    l");
+    print ("l______________________________________________________l");
+
+    opcion=int (input (("Ingrese opcion:    ")));
+    
+    if (opcion==1): 
+        """ IngresoDeProductos()"""
+        IngresoDeProd()
+        
+    elif (opcion==2):
+        ingreso()
+        resta()
+         
+    elif (opcion==3):
+        ingreso()
+        multiplicacion()
+           
+    elif (opcion==4):
+        ingreso()
+        division()
+        
+    elif (opcion==5):
+        break
